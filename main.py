4ab3e5221d1c3ab330d1cf983abd90a0f4c09e1b
Zyd Mohammad <93226542+zydmohd@users.noreply.github.com>
2023-03-04 11:45:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This entrypoint file to be used in development. Start by reading README.md
+
 import mean_var_std
 from unittest import main
 
@@ -7,6 +7,5 @@ print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
 
 
-# Run unit tests automatically
-
-main(module='test_module', exit=False)
+# Run unit tests 
+#main(module='test_module', exit=False)
